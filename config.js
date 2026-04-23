740051497607938b16c64f6e793f4860e1658d80
kiborg121217 <95975432+kiborg121217@users.noreply.github.com>
2026-04-24 00:19:24 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 window.KUBA_API_BASE = 'https://kuba-admin-api.onrender.com';
-=======
-window.KUBA_API_BASE = 'https://kuba-admin-api.onrender.com';
->>>>>>> 9873da706c67523cfaafac7880e5984c06e38505
